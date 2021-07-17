4514f59c1b87672ac43fff6847288521c5ec2f25
robert-katatumbo <robertbelov228@gmail.com>
2021-07-17 19:01:22 +0300

    .

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
-gcc -Wall -Wextra -O2 funny_char_sort.c -o funny_char_sort
+gcc -Wall -Wextra -O2 counting_sort.c -o counting_sort
 gcc -Wall -Wextra -O2 sort.c -o sort
 gcc -Wall -Wextra -O2 inverse_count.c -o inverse_count
